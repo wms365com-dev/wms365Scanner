a5fd6eb3029337bb99a1b7ebf094baf793e09e50
wms365com-dev <wms365com@gmail.com>
2026-05-22 14:13:47 -0400

    Fix Android fullscreen startup crash

--- android-app/app/src/main/java/com/wms365/app/MainActivity.kt
+++ android-app/app/src/main/java/com/wms365/app/MainActivity.kt
@@ -87,14 +87,16 @@ class MainActivity : Activity() {
     }
 
     private fun configureFullscreen() {
+        @Suppress("DEPRECATION")
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.insetsController?.hide(WindowInsets.Type.statusBars())
-            window.insetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        } else {
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            window.decorView.post {
+                window.insetsController?.hide(WindowInsets.Type.statusBars())
+                window.insetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
         }
     }
 
